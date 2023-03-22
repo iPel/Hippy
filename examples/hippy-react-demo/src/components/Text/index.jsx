04823be064331adddf2477c432ed1ecfceab34b0
iPel <pel20121221@gmail.com>
2023-03-22 21:29:00 +0800

    feat(android, ios): nested rich text support vertical alignment setting
    * feat(ios): rich text images support vertical alignment setting
    
    * feat(ios): add rich text's vertical align offset support
    
    * feat(ios): margin support for nested component in richText
    
    * feat(android): add `verticalAlign` for `Image` in rich text
    
    * fix(android): backgroundColor not working in nested text
    
    * feat(android): add properties for `Image` in rich text
    
    `tintColor`, `backgroundColor`, `margin` and `padding`
    
    * feat(ios): text in rich text support verticalAlign setting
    
    * feat(android): verticalAlign for textnode
    
    * feat(android): add backgroundColor in legacy mode
    
    * fix(android): baseline align offset wrong
    
    * feat(ios): support nested text verticalAlign style setting
    
    * fix(android): verticalAlign for boringLayout
    
    * chore(docs): add verticalAlign docs
    
    * chore(ios): make verticalAlign default value baseline
    
    * feat(vue): update text demo
    
    * chore(ios): add verticalAlign and update demo
    
    * chore(docs): update verticalAlign minimum supported version
    
    ---------
    
    Co-authored-by: iPel <pel20121221@gmail.com>
    Co-authored-by: luomy <ozonelmy@163.com>
    Co-authored-by: OpenHippy <124017524+open-hippy@users.noreply.github.com>

--- examples/hippy-react-demo/src/components/Text/index.jsx
+++ examples/hippy-react-demo/src/components/Text/index.jsx
@@ -8,6 +8,8 @@ import {
 } from '@hippy/react';
 
 const imgURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGAAAAAtCAMAAABmgJ64AAAAOVBMVEX/Rx8AAAD/QiL/Tif/QyH/RR//QiH/QiP/RCD/QSL/Qxz/QyH/QiL/QiD/QyL/QiL/QiH/QyH/QiLwirLUAAAAEnRSTlMZAF4OTC7DrWzjI4iietrRk0EEv/0YAAAB0UlEQVRYw72Y0Y6sIAxAKwUFlFH7/x97izNXF2lN1pU5D800jD2hJAJCdwYZuAUyVbmToKh903IhQHgErAVH+ccV0KI+G2oBPMxJgPA4WAigAT8F0IRDgNAE3ARyfeMFDGSc3YHVFkTBAHKDAgkEyHjacae/GTjxFqAo8NbakXrL9DRy9B+BCQwRcXR9OBKmEuAmAFFgcy0agBnIc1xZsMPOI5loAoUsQFmQjDEL9YbpaeGYBMGRKKAuqFEFL/JXApCw/zFEZk9qgbLGBx0gXLISxT25IUBREEgh1II1fph/IViGnZnCcDDVAgfgVg6gCy6ZaClySbDQpAl04vCGaB4+xGcFRK8CLvW0IBb5bQGqAlNwU4C6oEIVTLTcmoEr0AWcpKsZ/H0NAtkLQffnFjkOqiC/TTWBL9AFCwXQBHgI7rXImMgjCZwFa50s6DRBXyALmIECuMASiWNPFgRTgSJwM+XW8PDCmbwndzdaNL8FMYXPNjASDVChnIvWlBI/MKadPV952HszbmXtRERhhQ0vGFA52SVSSVt7MjHvxfRK8cdTpqovn02dUcltMrwiKf+wQ1FxXKCk9en6e/eDNnP44h2thQEb35O/etNv/q3iHza+KuhqqhZAAAAAAElFTkSuQmCC';
+const imgURL2 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAANlBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAC3dmhyAAAAEXRSTlMA9QlZEMPc2Mmmj2VkLEJ4Rsx+pEgAAAChSURBVCjPjVLtEsMgCDOAdbbaNu//sttVPes+zvGD8wgQCLp/TORbUGMAQtQ3UBeSAMlF7/GV9Cmb5eTJ9R7H1t4bOqLE3rN2UCvvwpLfarhILfDjJL6WRKaXfzxc84nxAgLzCGSGiwKwsZUB8hPorZwUV1s1cnGKw+yAOrnI+7hatNIybl9Q3OkBfzopCw6SmDVJJiJ+yD451OS0/TNM7QnuAAbvCG0TSAAAAABJRU5ErkJggg==';
+const imgURL4 = 'https://user-images.githubusercontent.com/12878546/148736255-7193f89e-9caf-49c0-86b0-548209506bd6.gif';
 
 const styles = StyleSheet.create({
   itemTitle: {
@@ -24,7 +26,6 @@ const styles = StyleSheet.create({
   itemContent: {
     alignItems: 'flex-start',
     justifyContent: 'center',
-    height: 100,
     borderWidth: 1,
     borderRadius: 2,
     borderColor: '#e0e0e0',
@@ -189,6 +190,81 @@ export default class TextExpo extends React.Component {
             <Image style={{ width: 70, height: 35 }} source={{ uri: imgURL }} />
             <Text numberOfLines={1} style={styles.customFont}>前面有张图片</Text>
           </Text>
+        </View>
+        {renderTitle('verticalAlign')}
+        <View style={[styles.itemContent, { height: Platform.OS === 'android' ? 160 : 70 }]}>
+          <Text style={[styles.normalText,
+            { lineHeight: 50, backgroundColor: '#4c9afa', paddingHorizontal: 10, paddingVertical: 5 }]}>
+            <Image style={{ width: 24, height: 24, verticalAlign: 'top' }} source={{ uri: imgURL2 }} />
+            <Image style={{ width: 18, height: 12, verticalAlign: 'middle' }} source={{ uri: imgURL2 }} />
+            <Image style={{ width: 24, height: 12, verticalAlign: 'baseline' }} source={{ uri: imgURL2 }} />
+            <Image style={{ width: 36, height: 24, verticalAlign: 'bottom' }} source={{ uri: imgURL2 }} />
+            <Image style={{ width: 24, height: 24, verticalAlign: 'top' }} source={{ uri: imgURL4 }} />
+            <Image style={{ width: 18, height: 12, verticalAlign: 'middle' }} source={{ uri: imgURL4 }} />
+            <Image style={{ width: 24, height: 12, verticalAlign: 'baseline' }} source={{ uri: imgURL4 }} />
+            <Image style={{ width: 36, height: 24, verticalAlign: 'bottom' }} source={{ uri: imgURL4 }} />
+            <Text style={{ fontSize: 16, verticalAlign: 'top' }}>字</Text>
+            <Text style={{ fontSize: 16, verticalAlign: 'middle' }}>字</Text>
+            <Text style={{ fontSize: 16, verticalAlign: 'baseline' }}>字</Text>
+            <Text style={{ fontSize: 16, verticalAlign: 'bottom' }}>字</Text>
+          </Text>
+          {Platform.OS === 'android' && (<>
+            <Text>legacy mode:</Text>
+            <Text style={[styles.normalText,
+              { lineHeight: 50, backgroundColor: '#4c9afa', marginBottom: 10, paddingHorizontal: 10, paddingVertical: 5 }]}>
+              <Image style={{ width: 24, height: 24, verticalAlignment: 0 }} source={{ uri: imgURL2 }} />
+              <Image style={{ width: 18, height: 12, verticalAlignment: 1 }} source={{ uri: imgURL2 }} />
+              <Image style={{ width: 24, height: 12, verticalAlignment: 2 }} source={{ uri: imgURL2 }} />
+              <Image style={{ width: 36, height: 24, verticalAlignment: 3 }} source={{ uri: imgURL2 }} />
+              <Image style={{ width: 24, height: 24, top: -10 }} source={{ uri: imgURL4 }} />
+              <Image style={{ width: 18, height: 12, top: -5 }} source={{ uri: imgURL4 }} />
+              <Image style={{ width: 24, height: 12 }} source={{ uri: imgURL4 }} />
+              <Image style={{ width: 36, height: 24, top: 3 }} source={{ uri: imgURL4 }} />
+              <Text style={{ fontSize: 16 }}>字</Text>
+              <Text style={{ fontSize: 16 }}>字</Text>
+              <Text style={{ fontSize: 16 }}>字</Text>
+              <Text style={{ fontSize: 16 }}>字</Text>
+            </Text>
+          </>)}
+        </View>
+        {renderTitle('tintColor & backgroundColor')}
+        <View style={[styles.itemContent]}>
+          <Text style={[styles.normalText,
+            { lineHeight: 30, backgroundColor: '#4c9afa', paddingHorizontal: 10, paddingVertical: 5 }]}>
+            <Image style={{ width: 24, height: 24, verticalAlign: 'middle', tintColor: 'orange' }} source={{ uri: imgURL2 }} />
+            <Image style={{ width: 24, height: 24, verticalAlign: 'middle', tintColor: 'orange', backgroundColor: '#ccc' }} source={{ uri: imgURL2 }} />
+            <Image style={{ width: 24, height: 24, verticalAlign: 'middle', backgroundColor: '#ccc' }} source={{ uri: imgURL2 }} />
+            <Text style={{ verticalAlign: 'middle', backgroundColor: '#090' }}>text</Text>
+          </Text>
+          {Platform.OS === 'android' && (<>
+            <Text>legacy mode:</Text>
+            <Text style={[styles.normalText,
+              { lineHeight: 30, backgroundColor: '#4c9afa', marginBottom: 10, paddingHorizontal: 10, paddingVertical: 5 }]}>
+              <Image style={{ width: 24, height: 24, tintColor: 'orange' }} source={{ uri: imgURL2 }} />
+              <Image style={{ width: 24, height: 24, tintColor: 'orange', backgroundColor: '#ccc' }} source={{ uri: imgURL2 }} />
+              <Image style={{ width: 24, height: 24, backgroundColor: '#ccc' }} source={{ uri: imgURL2 }} />
+            </Text>
+          </>)}
+        </View>
+        {renderTitle('margin')}
+        <View style={[styles.itemContent]}>
+          <Text style={[
+            { lineHeight: 50, backgroundColor: '#4c9afa', marginBottom: 5 }]}>
+            <Image style={{ width: 24, height: 24, verticalAlign: 'top', backgroundColor: '#ccc', margin: 5 }} source={{ uri: imgURL2 }} />
+            <Image style={{ width: 24, height: 24, verticalAlign: 'middle', backgroundColor: '#ccc', margin: 5 }} source={{ uri: imgURL2 }} />
+            <Image style={{ width: 24, height: 24, verticalAlign: 'baseline', backgroundColor: '#ccc', margin: 5 }} source={{ uri: imgURL2 }} />
+            <Image style={{ width: 24, height: 24, verticalAlign: 'bottom', backgroundColor: '#ccc', margin: 5 }} source={{ uri: imgURL2 }} />
+          </Text>
+          {Platform.OS === 'android' && (<>
+            <Text>legacy mode:</Text>
+            <Text style={[styles.normalText,
+              { lineHeight: 50, backgroundColor: '#4c9afa', marginBottom: 10, paddingHorizontal: 10, paddingVertical: 5 }]}>
+              <Image style={{ width: 24, height: 24, verticalAlignment: 0, backgroundColor: '#ccc', margin: 5 }} source={{ uri: imgURL2 }} />
+              <Image style={{ width: 24, height: 24, verticalAlignment: 1, backgroundColor: '#ccc', margin: 5 }} source={{ uri: imgURL2 }} />
+              <Image style={{ width: 24, height: 24, verticalAlignment: 2, backgroundColor: '#ccc', margin: 5 }} source={{ uri: imgURL2 }} />
+              <Image style={{ width: 24, height: 24, verticalAlignment: 3, backgroundColor: '#ccc', margin: 5 }} source={{ uri: imgURL2 }} />
+            </Text>
+          </>)}
         </View>
       </ScrollView>
     );
